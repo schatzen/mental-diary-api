0f239474f85a6198bf15def7a46ef7ce9927e24f
schatzen <gomieti@naver.com>
2021-01-04 10:18:04 +0900

    # GracefulShutdown

--- mental-api/src/main/java/com/mentaldiary/mentalapi/MentalApiApplication.java
+++ mental-api/src/main/java/com/mentaldiary/mentalapi/MentalApiApplication.java
@@ -1,21 +1,26 @@
 package com.mentaldiary.mentalapi;
 
+import com.mentaldiary.mentalapi.config.GracefulShutdown;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class MentalApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MentalApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MentalApiApplication.class, args);
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
+    @Bean
+    public GracefulShutdown gracefulShutdown() {
+        return new GracefulShutdown();
+    }
+
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//	}
 
 }
