146a9e5d84ac577667b1900858931be3767cd0ba
schatzen <56854068+schatzen@users.noreply.github.com>
2021-01-19 09:52:03 +0900

    # Post Like Entity 수정

--- mental-api/src/main/java/com/mentaldiary/mentalapi/entity/PostLike.java
+++ mental-api/src/main/java/com/mentaldiary/mentalapi/entity/PostLike.java
@@ -3,8 +3,7 @@ package com.mentaldiary.mentalapi.entity;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
@@ -12,5 +11,13 @@ import javax.persistence.Id;
 public class PostLike {
 
     @Id
+    @SequenceGenerator(name = "POST_LIKE_IDX_GEN", sequenceName = "POST_LIKE_IDX_SEQ", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "POST_LIKE_IDX_GEN")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="post_id")
+    private Post post;
+
+
 }
