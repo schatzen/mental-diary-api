69c767badd831ad232a86c94c0d6fb6ea9f82860
schatzen <56854068+schatzen@users.noreply.github.com>
2021-01-19 09:51:00 +0900

    Update MentalApiApplication.java

--- mental-api/src/main/java/com/mentaldiary/mentalapi/MentalApiApplication.java
+++ mental-api/src/main/java/com/mentaldiary/mentalapi/MentalApiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
@@ -20,9 +22,9 @@ public class MentalApiApplication {
         return new GracefulShutdown();
     }
 
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 
 }
